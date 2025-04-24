afc0b7706932f6ae273904292f2ddfae1f47ea07
ArtemMerkushyn <merkushyndev13@gmail.com>
2025-04-24 09:59:23 +0300

    wrote a simple bot  using an infinite while loop and an if else construct

--- chatBot.py
+++ chatBot.py
@@ -1,1 +1,23 @@
 print("Привет, Я chatBot! Давай поговорим!")
+print("Ты можешь написать: привет, как дела, пока")
+
+
+while True:
+    user_input = input("Я: ").lower()
+
+
+    if user_input == "пока":
+        print("ИИ: До встречи!")
+        break
+
+
+    elif "привет" in user_input:
+        print("ИИ: Привет! Рад тебя видеть.")
+
+    
+    elif "как дела" in user_input or "как ты" in user_input:
+        print("ИИ: У меня всё отлично! Спасибо что спросил.")
+
+    
+    else:
+        print("Извини, пока не понимаю эту фразу.")
