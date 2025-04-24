ed2c084f073cf78219a37cbab6566e65d636dcec
ArtemMerkushyn <merkushyndev13@gmail.com>
2025-04-24 10:10:54 +0300

    implemented name remembering

--- chatBot.py
+++ chatBot.py
@@ -2,21 +2,30 @@ print("Привет, Я chatBot! Давай поговорим!")
 print("Ты можешь написать: привет, как дела, пока")
 
 
+# Бот спрашивает имя и запоминает его
+user_name = input("ИИ: Как тебя зовут? ")
+print("ИИ: Приятно познакомиться, " + user_name + "!")
+
+
 while True:
     user_input = input("Я: ").lower()
 
 
     if user_input == "пока":
-        print("ИИ: До встречи!")
+        print("ИИ: До встречи, " + user_name + "!")
         break
 
 
     elif "привет" in user_input:
-        print("ИИ: Привет! Рад тебя видеть.")
+        print("ИИ: Привет, " + user_name + "! Рад тебя видеть.")
 
     
     elif "как дела" in user_input or "как ты" in user_input:
-        print("ИИ: У меня всё отлично! Спасибо что спросил.")
+        print("ИИ: У меня всё отлично! А у тебя, " + user_name + "?")
+
+    
+    elif "как меня зовут" in user_input:
+        print("Тебя зовут " + user_name + ".")
 
     
     else:
