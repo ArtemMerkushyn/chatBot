cd1f4fbf28bc393c324055ac07e18926549c712d
ArtemMerkushyn <merkushyndev13@gmail.com>
2025-04-24 10:55:33 +0300

    implemented saving of what I tought the bot in a separete file in json format

--- chatBot.py
+++ chatBot.py
@@ -1,15 +1,24 @@
+import os # библиотека для работы с файлами
+import json
+
+
 print("Привет, Я chatBot! Давай познакомимся!")
-# Бот спрашивает имя и запоминает его
 user_name = input("ИИ: Как тебя зовут? ")
 print("ИИ: Приятно познакомиться, " + user_name + "!")
 
 
-knowledge = {
-    "привет": "Привет, " + user_name + "! Рад тебя видеть.",
-    "как дела": "У меня всё отлично! А у тебя?",
-    "как ты": "Я хорошо себя чувствую!",
-    "как меня зовут": "Тебя зовут " + user_name + "!"
-}
+memory_file  = "memory.json"
+knowledge = {}
+
+
+# Загрузка памяти
+if os.path.exists(memory_file):
+    with open(memory_file, "r", encoding="utf-8") as file:
+        knowledge = json.load(file)
+
+
+# Добавляем имя в память (перезаписываем каждый раз)
+knowledge["как меня зовут"] = "Тебя зовут, " + user_name + "."
 
 
 print("Можешь поговорить со мной, если надоест, напиши 'пока' что бы выйти.")
@@ -32,4 +41,11 @@ while True:
         print("ИИ: Я не знаю, как ответить на это. Научи меня!")
         new_answer = input("Как мне нужно отвечать на это? ")
         knowledge[user_input] = new_answer
+
+
+        # Сохраняем память в JSON-файл
+        with open(memory_file, "w", encoding="utf-8") as file:
+            json.dump(knowledge, file, ensure_ascii=False, indent=2)
+
+
         print("ИИ: Спасибо! Теперь я это запомнил.")
